6c27571f6c30da13278f2fa421b8639474ab7488
RutgerJTK <smartrutger@gmail.com>
2021-03-16 13:07:09 +0100

    error-fix

--- main_func.py
+++ main_func.py
@@ -4,7 +4,7 @@ import pandas
 def main():
     filename_labels = "data/Data_Entry_2017.csv"
     labels_file = pandas.read_csv(filename_labels, sep=',', engine="python")
-    labels_file["Finding_Labels"] = labels_file["Finding_Labels"].map(lambda Finding_Labels: Finding_Labels.split("|"))
+    labels_file["Finding_Labels"] = labels_file["Finding_Labels"].map(lambda Finding_Labels: Finding_Labels.split("|"))     ## bevat de labels benodigd voor classificatie ~ röttie
  
     train_test_split(filename_labels)
     check_labels(labels_file)
@@ -15,5 +15,6 @@ def train_test_split(filename_labels):
 
 def check_labels(labels_file):
     label_checker.label_go_check(labels_file)
+
 
 main()
