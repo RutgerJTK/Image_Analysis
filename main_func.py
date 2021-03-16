43bbe820c679aa9aa9cbda53ff71b44c6aac3530
Annemieke <a.schonthaler@student.han.nl>
2021-03-16 12:43:58 +0100

    oeps

--- main_func.py
+++ main_func.py
@@ -1,7 +1,11 @@
 from src import split_stratified
+import pandas
 
 def main():
     filename_labels = "data/Data_Entry_2017.csv"
+    labels_file = pandas.read_csv(filename_labels, sep=',', engine="python")
+    labels_file["Finding_Labels"] = labels_file["Finding_Labels"].map(lambda Finding_Labels: Finding_Labels.split("|"))
+ 
     train_test_split(filename_labels)
 
 def train_test_split(filename_labels):
--- src/split_stratified.py
+++ src/split_stratified.py
@@ -1,7 +1,4 @@
-import pandas
 from skmultilearn.model_selection import iterative_train_test_split
 
 def call_stratified(labels_filename):
-    labels_file = pandas.read_csv(labels_filename, sep=',', engine="python")
-    labels_file["Finding_Labels"] = labels_file["Finding_Labels"].map(lambda Finding_Labels: Finding_Labels.split("|"))
-    print("kip")
+   print("kip")
