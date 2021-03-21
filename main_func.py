9b96c6c3e3a8b6e76a44919984f055085dad8dd7
RutgerJTK <smartrutger@gmail.com>
2021-03-21 23:19:51 +0100

    sorry for filename_labels stuff

--- main_func.py
+++ main_func.py
@@ -1,11 +1,15 @@
 from src import split_stratified, label_checker
 import pandas
+import collections
+import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
-    try:
-        filename_labels = "/data/Data_Entry_2017.csv"   	   ## pathing problem
-    except FileNotFoundError:
-        filename_labels = "D:\School - all things school related\HAN Bio-informatica\Jaar 3\Bi10\Image Analysis\VS_ImageAnalysis\Image_Analysis\data\Data_Entry_2017.csv" ## fixt rutger's path problem
+    filename_labels = "D:\School - all things school related\HAN Bio-informatica\Jaar 3\Bi10\Image Analysis\VS_ImageAnalysis\Image_Analysis\data\Data_Entry_2017.csv"
+    # try:
+    #     filename_labels = "/data/Data_Entry_2017.csv"   	   ## pathing problem
+    # except FileNotFoundError:
+    #     filename_labels = "D:\School - all things school related\HAN Bio-informatica\Jaar 3\Bi10\Image Analysis\VS_ImageAnalysis\Image_Analysis\data\Data_Entry_2017.csv" ## fixt rutger's path problem
     labels_file = pandas.read_csv(filename_labels, sep=',', engine="python")
     labels_file["Finding_Labels"] = labels_file["Finding_Labels"].map(lambda Finding_Labels: Finding_Labels.split("|"))     ## bevat de labels benodigd voor classificatie ~ röttie
  
@@ -18,6 +22,6 @@ def train_test_split(filename_labels):
 
 def check_labels(labels_file):
     label_checker.label_go_check(labels_file)
-
+    print('I did sumthin and it sorta wurks ¯\_(ツ)_/¯ yay I guess!')
 
 main()
