c6643038fbd3b222b2b775c3774ab27be88b288c
RutgerJTK <smartrutger@gmail.com>
2021-03-21 20:15:23 +0100

    try except added

--- main_func.py
+++ main_func.py
@@ -2,7 +2,10 @@ from src import split_stratified, label_checker
 import pandas
 
 def main():
-    filename_labels = "data/Data_Entry_2017.csv"
+    try:
+        filename_labels = "/data/Data_Entry_2017.csv"   	   ## pathing problem
+    except FileNotFoundError:
+        filename_labels = "D:\School - all things school related\HAN Bio-informatica\Jaar 3\Bi10\Image Analysis\VS_ImageAnalysis\Image_Analysis\data\Data_Entry_2017.csv" ## fixt rutger's path problem
     labels_file = pandas.read_csv(filename_labels, sep=',', engine="python")
     labels_file["Finding_Labels"] = labels_file["Finding_Labels"].map(lambda Finding_Labels: Finding_Labels.split("|"))     ## bevat de labels benodigd voor classificatie ~ röttie
  
